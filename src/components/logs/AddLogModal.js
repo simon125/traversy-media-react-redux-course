9ff23a74ed66af940596e4aae0b3ddb99336b08a
Szymon Oleszek <s.oleszek@polsource.com>
2019-08-23 15:03:02 +0200

    Intro to the newest cuurse by Travesy Media
    - setup of fake sever
    - setup ui stuff

---
+++ src/components/logs/AddLogModal.js
@@ -0,0 +1,56 @@
+import React, { useState } from "react";
+import PropTypes from "prop-types";
+
+const AddLogModal = props => {
+  const [message, setMessage] = useState("");
+  const [attention, setAttention] = useState(false);
+  const [tech, setTech] = useState("");
+
+  return (
+    <div id="add-log-modal" className="modal" style={modalStyle}>
+      <div className="modal-content">
+        <h4>Enter System Log</h4>
+        <div className="row">
+          <div className="input-field">
+            <input
+              type="text"
+              name="message"
+              value={message}
+              onChange={e => setMessage(e.target.value)}
+            />
+            <label htmlFor="message" className="active">
+              Log Message
+            </label>
+          </div>
+        </div>
+        <div className="row">
+          <div className="input-field">
+            <select
+              name="tech"
+              value={tech}
+              className="browser-default"
+              onChange={e => setTech(e.target.value)}
+            >
+              <option value="" disabled>
+                Select Technician
+              </option>
+              <option value="John Doe">John Doe</option>
+              <option value="Sam Smith">Sam Smith</option>
+              <option value="Sara Wilson">Sara Wilson</option>
+            </select>
+          </div>
+        </div>
+        <div className="row" />
+      </div>
+    </div>
+  );
+};
+
+AddLogModal.propTypes = {};
+
+const modalStyle = {
+  width: "75%",
+  height: "75%"
+};
+
+export default AddLogModal;
